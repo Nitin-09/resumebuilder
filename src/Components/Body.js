c4195f8d502f5c83870d456b6b0906050ba15d38
Nitin-09 <nitingangwani982000@gmail.com>
2022-10-25 17:41:27 +0530

    50% done

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
 function Body() {
+    const color=["bg-orange-500","bg-green-500","bg-yellow-500","bg-blue-500 "]
   return (
-    <div className='m-4 flex items-center justify-center'>
-        <p className='font-bold text-md'>Resume Builder</p>
+    <div className='m-4'>
+        <p className='text-center font-bold text-md'>Resume Builder</p>
+        <div>
+            <div className='flex space-x-2'>
+                {color.map(item=>
+                <span key={item} className={`${item} flex p-3 rounded-full cursor-pointer hover:border border-black`}></span>)}
+            </div>
+            <div><button>Download</button></div>
+        </div>
     </div>
   )
 }
