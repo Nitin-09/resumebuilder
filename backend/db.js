670057ba0ec0b46b2aebee9281b66cfd323ab81b
Nitin-09 <nitingangwani982000@gmail.com>
2023-04-09 13:16:28 +0530

    Backend Seprated

--- backend/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-const mongoURI ="mongodb://localhost:27017/ResumeDatabase"
-const connectToMongo =()=>{
-    mongoose.connect(mongoURI,()=>{
-        console.log("connected")
-    })
-}
-module.exports=connectToMongo;
--- backend/middleware/imageUpload.js
+++
@@ -1,5 +0,0 @@
-const cloudinary = require('cloudinary').v2;
-
-cloudinary.v2.uploader
-.upload("/home/my_image.jpg")
-.then(result=>console.log(result));
