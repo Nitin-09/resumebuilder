6396e010799c253ae5715c6d29b84d858abe8de6
Nitin-09 <nitingangwani982000@gmail.com>
2023-04-26 15:46:30 +0530

    profile component

--- src/Components/Profiles.js
+++ src/Components/Profiles.js
@@ -1,7 +1,9 @@
 import React, { useContext, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
 import ResumeContext from "../context/Resume/ResumeContext.js";
 
 function Profiles() {
+    let navigate = useNavigate()
     const context = useContext(ResumeContext)
     const { fetchAllResume, ResumeData } = context
     useEffect(() => {
@@ -11,48 +13,57 @@ function Profiles() {
     useEffect(() => {
         console.log(ResumeData)
     }, [ResumeData])
+    const calculatePercentage =(element) => {
+    let i=0;
+    if(element.profile[0].firstName)
+    i=i+1
+    if(element.education[0].details.length>0)
+    i=i+1
+    if(element.workExperience[0].details.length>0)
+    i=i+1
+    if(element.project[0].details.length>0)
+    i=i+1
+    if(element.skills[0].details.length>0)
+    i=i+1
+    if(element.summary[0].aboutMe)
+    i=i+1
+    return Math.floor((i*100)/6)
+    }
+
     return (
-        <div className='flex-col gap-5 h-fit bg-gradient-to-r from-[#5134df] to-[#c056e7] flex items-center p-5 lg:p-10 overflow-hidden'>
+        <div className='flex flex-wrap justify-center gap-5 h-fit bg-gradient-to-r from-[#5134df] to-[#c056e7] p-5 lg:p-10 overflow-hidden'>
             {
                 ResumeData.map((element, index) => {
                     return (
-                        <div className="max-w-4xl flex items-center h-auto lg:h-screen mx-auto my-32 lg:my-0">
-                            <div id="profile"
-                                className="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none bg-white shadow-2xl shadow-black mx-6 lg:mx-0">
-                                <div className="p-4 md:p-12 text-center lg:text-left">
-                                    <div className="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center"></div>
-                                    <h1 className="text-3xl font-bold pt-8 lg:pt-0">{element.profile[0].firstName} {element.profile[0].lastName}</h1>
-                                    <div className="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-green-500 opacity-25"></div>
-                                    <p className="pt-4 text-base font-bold flex items-center justify-center lg:justify-start">
-                                        <svg className="h-4 fill-current text-green-700 pr-4" xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 20 20">
-                                            <path
-                                                d="M9 12H1v6a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-6h-8v2H9v-2zm0-1H0V5c0-1.1.9-2 2-2h4V2a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v1h4a2 2 0 0 1 2 2v6h-9V9H9v2zm3-8V2H8v1h4z" />
-                                        </svg>{element.profile[0].title}
-                                    </p>
-                                    <p className="pt-2 text-gray-600 text-xs lg:text-sm flex items-center justify-center lg:justify-start">
-                                        <svg className="h-4 fill-current text-green-700 pr-4" xmlns="http://www.w3.org/2000/svg"
-                                            viewBox="0 0 20 20">
-                                            <path
-                                                d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm7.75-8a8.01 8.01 0 0 0 0-4h-3.82a28.81 28.81 0 0 1 0 4h3.82zm-.82 2h-3.22a14.44 14.44 0 0 1-.95 3.51A8.03 8.03 0 0 0 16.93 14zm-8.85-2h3.84a24.61 24.61 0 0 0 0-4H8.08a24.61 24.61 0 0 0 0 4zm.25 2c.41 2.4 1.13 4 1.67 4s1.26-1.6 1.67-4H8.33zm-6.08-2h3.82a28.81 28.81 0 0 1 0-4H2.25a8.01 8.01 0 0 0 0 4zm.82 2a8.03 8.03 0 0 0 4.17 3.51c-.42-.96-.74-2.16-.95-3.51H3.07zm13.86-8a8.03 8.03 0 0 0-4.17-3.51c.42.96.74 2.16.95 3.51h3.22zm-8.6 0h3.34c-.41-2.4-1.13-4-1.67-4S8.74 3.6 8.33 6zM3.07 6h3.22c.2-1.35.53-2.55.95-3.51A8.03 8.03 0 0 0 3.07 6z" />
-                                        </svg> {element.profile[0].address}
-                                    </p>
-                                    <p className="pt-8 text-xs">{element.summary[0].aboutMe}</p>
-
-                                    <div className="pt-12 pb-8">
-                                        <button className="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded-full">
-                                            View Resume
-                                        </button>
-                                    </div>
-
-                                </div>
-
+                        <section class="w-64 mx-auto bg-[#0b2d54] rounded-2xl px-8 py-6 shadow-lg">
+                            <div class="flex items-center justify-between">
+                                <span class="text-gray-400 text-sm">2d ago</span>
+                                <span class="text-emerald-400">
+                                   <button className='hover:underline' onClick={()=>{navigate(`/newresume/${element.tempelateId}/${element._id}`)}}>Edit Resume</button>
+                                </span>
                             </div>
-                            <div className="h-100">
-                                <img src={element.profile[0].URL} classname="rounded-none lg:rounded-lg shadow-2xl shadow-black h-[80vh]" alt="" />
-
+                            <div class="mt-6 w-fit mx-auto">
+                                <img src={element.profile[0].URL} class="rounded-full w-44 h-44 " alt="profile picture" srcset="" />
                             </div>
-                        </div>
+
+                            <div class="mt-8 ">
+                                <h2 class="text-white font-bold text-2xl tracking-wide">{element.profile[0].firstName} <br /> {element.profile[0].lastName}</h2>
+                            </div>
+                            <p class="text-emerald-400 font-semibold mt-2.5" >
+                                Profile Incomplete
+                            </p>
+
+                            <div class="h-1 w-full bg-black mt-8 rounded-full">
+                                <div class={`h-1 rounded-full w-[${calculatePercentage(element)}%] bg-yellow-500 `}></div>
+                            </div>
+                            <div class="mt-3 text-white text-sm">
+                                <span class="text-gray-400 font-semibold">Status:</span>
+                                <span>
+                                {calculatePercentage(element)}%
+                                </span>
+                            </div>
+
+                        </section>
                     )
                 })
             }
